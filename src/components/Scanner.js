56f51f730e0eb1ebcf7e31c594c1b1455adfac13
Minestorm01 <112302130+Minestorm01@users.noreply.github.com>
2025-06-05 09:55:43 +1000

    Update Scanner.js

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+//import './Scanner.css';
 
 function Scanner({ onCompleteScan, csvData, onCsvChange }) {
   const [scannedSKUs, setScannedSKUs] = useState([]);
