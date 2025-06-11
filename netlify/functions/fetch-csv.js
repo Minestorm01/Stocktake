59f595e874a923ed97e3027bcd04831e26b938c0
Minestorm01 <112302130+Minestorm01@users.noreply.github.com>
2025-06-11 14:02:31 +1000

    Update fetch-csv.js

--- netlify/functions/fetch-csv.js
+++ netlify/functions/fetch-csv.js
@@ -3,9 +3,12 @@ const fetch = require('node-fetch');
 exports.handler = async (event) => {
   const filename = event.queryStringParameters.filename;
   const token = process.env.GITHUB_TOKEN;
+  const repoOwner = process.env.REPO_OWNER;
+  const repoName = process.env.REPO_NAME;
 
-  const repoOwner = "YOUR_USERNAME";
-  const repoName = "YOUR_REPO";
+  if (!repoOwner || !repoName) {
+    return { statusCode: 200, body: JSON.stringify({ disabled: true }) };
+  }
   const url = `https://raw.githubusercontent.com/${repoOwner}/${repoName}/main/${filename}`;
 
   const res = await fetch(url, {
